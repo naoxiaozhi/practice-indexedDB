b5bfa2872b99f683e8c220ffe487f5cb4d2b9920
naoxiaozhi <zzy_come_on@163.com>
2017-01-20 15:02:08 +0800

    Update data.js

--- data.js
+++ data.js
@@ -9,13 +9,13 @@ var data_table=[
 	{ number: "20103924", name: "Donna", age: 32, sex: "男" } ,
 	{ number: "20104006", name: "zzy", age: 23, sex: "女" },   
 	{ number: "20103854", name: "闹小志", age: 22, sex: "女" }, 
-	{ number: "20103006", name: "张志影", age: 33, sex: "女" },   
-	{ number: "20114006", name: "申向玉", age: 67, sex: "男" }, 
-	{ number: "20124006", name: "王雪微", age: 34, sex: "女" },   
-	{ number: "20134006", name: "高广达", age: 89, sex: "男" }, 
-	{ number: "20144006", name: "王艳超", age: 11, sex: "男" },   
-	{ number: "20104016", name: "宋佳佳", age: 24, sex: "女" }, 
-	{ number: "20103745", name: "贾晓霞", age: 25, sex: "女" },   
-	{ number: "20103774", name: "陈军", age: 46, sex: "男" } ,
-	{ number: "20103886", name: "郭宏", age: 67, sex: "男" }
+	{ number: "20103006", name: "张小影", age: 33, sex: "女" },   
+	{ number: "20114006", name: "张小二", age: 67, sex: "男" }, 
+	{ number: "20124006", name: "王小微", age: 34, sex: "女" },   
+	{ number: "20134006", name: "高小达", age: 89, sex: "男" }, 
+	{ number: "20144006", name: "王小超", age: 11, sex: "男" },   
+	{ number: "20104016", name: "宋小佳", age: 24, sex: "女" }, 
+	{ number: "20103745", name: "贾小蕾", age: 25, sex: "女" },   
+	{ number: "20103774", name: "任小静", age: 20, sex: "女" } ,
+	{ number: "20103886", name: "郭小宏", age: 67, sex: "男" }
 ];
